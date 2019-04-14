9ac8b1722be5d6e8154816564458729db88a18e8
xpendence <slava_rossii@list.ru>
2019-04-14 21:38:34 +0300

    Initial commit

---
+++ src/main/java/ru/xpendence/rebounder/RebounderChainBackendApplication.java
@@ -0,0 +1,13 @@
+package ru.xpendence.rebounder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RebounderChainBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RebounderChainBackendApplication.class, args);
+    }
+
+}
---
+++ src/main/java/ru/xpendence/rebounder/entity/RequestLog.java
@@ -0,0 +1,37 @@
+package ru.xpendence.rebounder.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+/**
+ * Author: Vyacheslav Chernyshov
+ * Date: 14.04.19
+ * Time: 21:20
+ * e-mail: 2262288@gmail.com
+ */
+@Setter
+@Entity
+@Table(name = "request_logs")
+@NoArgsConstructor
+@AllArgsConstructor
+public class RequestLog implements Serializable {
+
+    private Long id;
+    private LocalDateTime created;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long getId() {
+        return id;
+    }
+
+    @Column
+    public LocalDateTime getCreated() {
+        return created;
+    }
+}
