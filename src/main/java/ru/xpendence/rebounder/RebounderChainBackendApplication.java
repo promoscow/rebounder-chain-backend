8d9df9bab1d5b91e75c45a85b204ac41decd410e
v.chernyshov <v.chernyshov@pflb.ru>
2019-04-16 18:52:16 +0300

    - 2 реализации отключения CORS, которые не пригодились

--- src/main/java/ru/xpendence/rebounder/RebounderChainBackendApplication.java
+++ src/main/java/ru/xpendence/rebounder/RebounderChainBackendApplication.java
@@ -9,15 +9,4 @@ public class RebounderChainBackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(RebounderChainBackendApplication.class, args);
     }
-
-//    @Bean
-//    public WebMvcConfigurer corsConfigurer() {
-//        return new WebMvcConfigurer() {
-//            @Override
-//            public void addCorsMappings(CorsRegistry registry) {
-//                registry.addMapping("/**")
-//                        .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
-//            }
-//        };
-//    }
 }
