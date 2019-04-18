c5b9bdb82145920544ece5426a86ee6ea2fe0683
xpendence <slava_rossii@list.ru>
2019-04-18 22:31:03 +0300

    ready

--- src/main/java/ru/xpendence/rebounder/controller/LogController.java
+++ src/main/java/ru/xpendence/rebounder/controller/LogController.java
@@ -2,8 +2,7 @@ package ru.xpendence.rebounder.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.xpendence.rebounder.entity.Log;
@@ -26,8 +25,8 @@ public class LogController {
         this.repository = repository;
     }
 
-    @PostMapping
-    public ResponseEntity<Log> save(@RequestBody Log log) {
-        return ResponseEntity.ok(repository.save(log));
+    @GetMapping
+    public ResponseEntity<Log> log() {
+        return ResponseEntity.ok(repository.save(new Log()));
     }
 }
