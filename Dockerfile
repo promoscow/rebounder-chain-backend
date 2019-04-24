701bd4e9eb215a229f4ffbd4f697a924e732f56a
xpendence <slava_rossii@list.ru>
2019-04-24 21:44:54 +0300

    ready to publish

--- Dockerfile
+++ Dockerfile
@@ -2,21 +2,10 @@
 FROM openjdk:8-jdk-alpine
 
 LABEL maintainer="2262288@gmail.com"
-
-WORKDIR /home/vchernyshov/IdeaProjects/rebounder-chain-backend
 VOLUME /tmp
 
-EXPOSE 8088
+EXPOSE 8080
 
-ARG JAR_FILE=rebounder-chain-backend/build/libs/rebounder-chain-backend-0.0.1-SNAPSHOT.jar
+ARG JAR_FILE=build/libs/rebounder-chain-backend-0.0.1-SNAPSHOT.jar
 ADD ${JAR_FILE} rebounder-chain-backend.jar
 ENTRYPOINT ["java","-jar","/rebounder-chain-backend.jar"]
-
-# front
-FROM node:alpine
-LABEL maintainer="2262288@gmail.com"
-WORKDIR /home/vchernyshov/IdeaProjects/rebounder-chain-backend
-COPY ./ ./
-
-RUN npm install
-CMD ["npm", "start"]
--- src/main/java/ru/xpendence/rebounder/entity/Log.java
+++ src/main/java/ru/xpendence/rebounder/entity/Log.java
@@ -1,14 +1,11 @@
 package ru.xpendence.rebounder.entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
 
 import javax.persistence.*;
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 /**
  * Author: Vyacheslav Chernyshov
@@ -16,16 +13,20 @@ import java.time.LocalDateTime;
  * Time: 21:20
  * e-mail: 2262288@gmail.com
  */
-@Setter
 @Entity
 @Table(name = "request_logs")
-@NoArgsConstructor
-@AllArgsConstructor
-@ToString
 public class Log implements Serializable {
 
     private Long id;
     private LocalDateTime created;
+
+    public Log() {
+    }
+
+    public Log(Long id, LocalDateTime created) {
+        this.id = id;
+        this.created = created;
+    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -42,5 +43,35 @@ public class Log implements Serializable {
     @PrePersist
     public void prePersist() {
         this.created = LocalDateTime.now();
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
+    }
+
+    @Override
+    public String toString() {
+        return "Log{" +
+                "id=" + id +
+                ", created=" + created +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Log log = (Log) o;
+        return Objects.equals(id, log.id) &&
+                Objects.equals(created, log.created);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, created);
     }
 }
